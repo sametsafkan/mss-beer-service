05367fd55b74eefa19367dc028b1fb929f46ffc6
sametsafkan <D6cf53ec36>
2020-04-12 14:45:33 +0300

    BeerDto, BeerPagedList, BeerStyle added.

---
+++ src/main/java/com/sametsafkan/mssbeerservice/web/model/BeerDto.java
@@ -0,0 +1,26 @@
+package com.sametsafkan.mssbeerservice.web.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.time.OffsetDateTime;
+import java.util.UUID;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class BeerDto {
+    private UUID id;
+    private Integer version;
+    private OffsetDateTime createDate;
+    private OffsetDateTime lastModifiedDate;
+    private String beerName;
+    private BeerStyle beerStyle;
+    private Long upc;
+    private BigDecimal price;
+    private Integer quantityOnHand;
+}
---
+++ src/main/java/com/sametsafkan/mssbeerservice/web/model/BeerStyle.java
@@ -0,0 +1,5 @@
+package com.sametsafkan.mssbeerservice.web.model;
+
+public enum BeerStyle {
+     LAGER, PILSENER, STOUT, GOSE, PORTER, ALE, WHEAT, IPA, PALE_ALE, SAISON
+}
