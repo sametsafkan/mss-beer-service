f686ed53df1087bdf9140e78cf2eebcc66f48547
sametsafkan <D6cf53ec36>
2020-04-13 19:19:03 +0300

    added validations.

--- src/main/java/com/sametsafkan/mssbeerservice/web/model/BeerDto.java
+++ src/main/java/com/sametsafkan/mssbeerservice/web/model/BeerDto.java
@@ -5,6 +5,10 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Null;
+import javax.validation.constraints.Positive;
 import java.math.BigDecimal;
 import java.time.OffsetDateTime;
 import java.util.UUID;
@@ -14,13 +18,23 @@ import java.util.UUID;
 @NoArgsConstructor
 @AllArgsConstructor
 public class BeerDto {
+    @Null
     private UUID id;
+    @Null
     private Integer version;
+    @Null
     private OffsetDateTime createDate;
+    @Null
     private OffsetDateTime lastModifiedDate;
+    @NotBlank
     private String beerName;
+    @NotNull
     private BeerStyle beerStyle;
+    @NotNull
+    @Positive
     private Long upc;
+    @NotNull
+    @Positive
     private BigDecimal price;
     private Integer quantityOnHand;
 }
