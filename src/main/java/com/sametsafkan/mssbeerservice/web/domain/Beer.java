96df17323b90326564f4006dc974db900633ea86
sametsafkan <D6cf53ec36>
2020-04-13 11:28:11 +0300

    Added Beer entity.

---
+++ src/main/java/com/sametsafkan/mssbeerservice/web/domain/Beer.java
@@ -0,0 +1,38 @@
+package com.sametsafkan.mssbeerservice.web.domain;
+
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+import java.sql.Timestamp;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+public class Beer {
+    @Id
+    @GeneratedValue(generator = "UUID")
+    @GenericGenerator(name = "UUID", strategy = "org.hibernate.id.UUIDGenerator")
+    @Column(length = 36, columnDefinition = "varchar", updatable = false, nullable = false)
+    private String id;
+    @Version
+    private String version;
+    @CreationTimestamp
+    @Column(updatable = false)
+    private Timestamp createDate;
+    @UpdateTimestamp
+    private Timestamp lastModifiedDate;
+    private String name;
+    private String style;
+    @Column(unique = true)
+    private Long upc;
+    private BigDecimal price;
+    private Integer minOnHand;
+    private Integer quantityToBrew;
+}
