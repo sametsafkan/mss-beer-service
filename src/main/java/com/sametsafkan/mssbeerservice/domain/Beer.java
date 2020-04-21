b52d8c26281302e9df4a03b6a220f9a5fd79be2a
Samet Safkan <sametsafkan@gmail.com>
2020-04-21 14:52:01 +0300

    local mysql configuration added.

--- src/main/java/com/sametsafkan/mssbeerservice/domain/Beer.java
+++ src/main/java/com/sametsafkan/mssbeerservice/domain/Beer.java
@@ -21,7 +21,7 @@ public class Beer {
     @Id
     @GeneratedValue(generator = "UUID")
     @GenericGenerator(name = "UUID", strategy = "org.hibernate.id.UUIDGenerator")
-    @Column(length = 36, columnDefinition = "varchar", updatable = false, nullable = false)
+    @Column(length = 36, columnDefinition = "varchar(36)", updatable = false, nullable = false)
     @Type(type = "org.hibernate.type.UUIDCharType")
     private UUID id;
     @Version
