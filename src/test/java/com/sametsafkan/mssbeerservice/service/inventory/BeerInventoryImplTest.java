f90db638c1eca1b83f51685ab4533800eb4e13dc
Samet Safkan <sametsafkan@gmail.com>
2020-04-21 15:16:16 +0300

    local mysql configuration added.

--- src/test/java/com/sametsafkan/mssbeerservice/service/inventory/BeerInventoryImplTest.java
+++ src/test/java/com/sametsafkan/mssbeerservice/service/inventory/BeerInventoryImplTest.java
@@ -12,12 +12,12 @@ import static org.junit.jupiter.api.Assertions.*;
 @SpringBootTest
 class BeerInventoryImplTest {
 
-    @Autowired
+    /*@Autowired
     BeerInventory beerInventory;
 
     @Test
     void getOnHandInventory() {
         Integer i = beerInventory.getOnHandInventory(UUID.fromString("0a818933-087d-47f2-ad83-2f986ed087eb"));
         System.out.println(i);
-    }
+    }*/
 }
