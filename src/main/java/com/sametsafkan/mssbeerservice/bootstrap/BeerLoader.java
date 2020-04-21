7732d23805b9281dda5c29ea9e1f4b6c51ab7553
Samet Safkan <sametsafkan@gmail.com>
2020-04-21 15:17:35 +0300

    changed upc values.

--- src/main/java/com/sametsafkan/mssbeerservice/bootstrap/BeerLoader.java
+++ src/main/java/com/sametsafkan/mssbeerservice/bootstrap/BeerLoader.java
@@ -11,9 +11,9 @@ import java.math.BigDecimal;
 @Component
 public class BeerLoader implements CommandLineRunner {
 
-    public static final String BEER_1_UPC = "0631234200037";
-    public static final String BEER_2_UPC = "0631234300018";
-    public static final String BEER_3_UPC = "0083783375214";
+    public static final String BEER_1_UPC = "0631234200036";
+    public static final String BEER_2_UPC = "0631234300019";
+    public static final String BEER_3_UPC = "0083783375213";
 
     private final BeerRepository beerRepository;
 
@@ -27,30 +27,32 @@ public class BeerLoader implements CommandLineRunner {
     }
 
     private void loadBeers() {
-        beerRepository.save(new Beer().builder()
-                .name("Tuborg")
-                .style(BeerStyle.PALE_ALE.toString())
-                .minOnHand(10)
-                .quantityToBrew(150)
-                .price(new BigDecimal(15.95))
-                .upc(BEER_1_UPC)
-                .build());
-        beerRepository.save(new Beer().builder()
-                .name("Efes")
-                .style(BeerStyle.PILSENER.toString())
-                .minOnHand(8)
-                .quantityToBrew(100)
-                .price(new BigDecimal(15.95))
-                .upc(BEER_2_UPC)
-                .build());
-        beerRepository.save(new Beer().builder()
-                .name("Corona")
-                .style(BeerStyle.PALE_ALE.toString())
-                .minOnHand(9)
-                .quantityToBrew(100)
-                .price(new BigDecimal(20.95))
-                .upc(BEER_3_UPC)
-                .build());
-        System.out.println(beerRepository.count());
+        long beerCount = beerRepository.count();
+        if(beerCount == 0){
+            beerRepository.save(new Beer().builder()
+                    .name("Tuborg")
+                    .style(BeerStyle.PALE_ALE.toString())
+                    .minOnHand(10)
+                    .quantityToBrew(150)
+                    .price(new BigDecimal(15.95))
+                    .upc(BEER_1_UPC)
+                    .build());
+            beerRepository.save(new Beer().builder()
+                    .name("Efes")
+                    .style(BeerStyle.PILSENER.toString())
+                    .minOnHand(8)
+                    .quantityToBrew(100)
+                    .price(new BigDecimal(15.95))
+                    .upc(BEER_2_UPC)
+                    .build());
+            beerRepository.save(new Beer().builder()
+                    .name("Corona")
+                    .style(BeerStyle.PALE_ALE.toString())
+                    .minOnHand(9)
+                    .quantityToBrew(100)
+                    .price(new BigDecimal(20.95))
+                    .upc(BEER_3_UPC)
+                    .build());
+        }
     }
 }
